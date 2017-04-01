80e78367357d027cac70a7b1ca8f3f8093e7340a
erroneousboat <jpbruinsslot@gmail.com>
2017-04-01 17:46:57 +0200

    battery: update script with notification

--- files/battery/tmux-battery.py
+++ files/battery/tmux-battery.py
@@ -1,24 +1,59 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import re
 import sys
 import subprocess
+from pathlib import Path
+
+FILE = "/tmp/bat"
+
+
+def notify(percentage):
+    touch_file = os.path.isfile(FILE)
+
+    if percentage > 80 and not touch_file:
+        # notify and create file
+        subprocess.run(
+            [
+                "notify-send",
+                "-i",
+                "battery",
+                "Please disconnect charger"
+            ]
+        )
+        Path(FILE).touch()
+    elif 40 <= percentage <= 80 and touch_file:
+        # remove touch file
+        os.remove(FILE)
+    elif percentage < 40 and not touch_file:
+        # notify and create file
+        subprocess.run(
+            [
+                "notify-send",
+                "-i",
+                "battery",
+                "Please connect charger"
+            ]
+        )
+        Path(FILE).touch()
+    else:
+        pass
 
 
 def check_status(metric_status):
-    if metric_status == "Full":
+    if metric_status == b"Full":
         return "✔"
-    elif metric_status == "Charging":
+    elif metric_status == b"Charging":
         return "⚡"
-    elif metric_status == "Discharging":
+    elif metric_status == b"Discharging":
         return "⇣"
     else:
         return "?"
 
 
 def main():
-    # Check battery status using acpi
     try:
         console_output = subprocess.check_output(
             "acpi",
@@ -28,17 +63,19 @@ def main():
         sys.exit(
             "acpi not found please install with sudo apt-get install acpi")
 
-    if "No support" in console_output:
-        # There is no battery and probably on a power supply
+    # There is no battery and probably on a power supply
+    if b"No support" in console_output:
         print("🔌")
         return
 
     # Format will be:
     # Battery 1: discharging, 44%, 00:18:48 remaining
-    list_metrics = console_output.split(", ")
+    list_metrics = console_output.split(b", ")
 
-    metric_status = list_metrics[0].split(":")[-1].strip()
-    metric_percentage = int(re.match(r'\d+', list_metrics[1]).group())
+    metric_status = list_metrics[0].split(b":")[-1].strip()
+    metric_percentage = int(re.match(b'\d+', list_metrics[1]).group())
+
+    notify(metric_percentage)
 
     print("%s %s%%" % (check_status(metric_status), metric_percentage))
 
