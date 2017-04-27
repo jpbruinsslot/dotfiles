fe12275c1831191ddfc3f1d978b8bea388ee202f
erroneousboat <jpbruinsslot@gmail.com>
2017-04-27 14:38:54 +0200

    bin: update external-ip script

--- files/bin/bin/external-ip.sh
+++ files/bin/bin/external-ip.sh
@@ -1,13 +1,34 @@
 #/bin/bash
-# https://github.com/vivien/i3blocks/blob/master/scripts/wifi
 
+##############################################################################
+#
+# external-ip
+# -----------
+#
+# This script will output the external IP address of the host machine by using
+# the dig command. It will accompany the IP address with an icon indicating
+# whether the host machine is connected by wifi or wire.
+#
+#
+# Dependencies: dig, font-awesome
+#
+# Credits:
+# - https://github.com/vivien/i3blocks/blob/master/scripts/wifi
+#
+# :authors: J.P.H. Bruins Slot
+# :date:    27-04-2017
+# :version: 0.1.0
+#
+##############################################################################
+
+INTERFACE="wlan0"
 IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 if [ -z "$IP" ]; then
     IP=""
 fi
 
-if [ ! -d /sys/class/net/wlan0/wireless ] || [ "$(cat /sys/class/net/wlan0/operstate)" = 'down' ]; then
+if [ ! -d /sys/class/net/${INTERFACE}/wireless ] || [ "$(cat /sys/class/net/${INTERFACE}/operstate)" = 'down' ]; then
     echo " ${IP}"
 else
     QUALITY=$(grep $INTERFACE /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
