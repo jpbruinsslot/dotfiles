ba30b238fa85553f8e4441f6c41e064f0419ef9b
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-01-02 18:05:39 +0100

    nvim: fix lsp signs

--- files/nvim/nvim/lua/jpbruinsslot/plugins/lsp/lspconfig.lua
+++ files/nvim/nvim/lua/jpbruinsslot/plugins/lsp/lspconfig.lua
@@ -71,7 +71,7 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 		-- change diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = " ", Information = " " }
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
