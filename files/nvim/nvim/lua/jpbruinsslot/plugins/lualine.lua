22d6ee6534f5f3353532c06cff7f837211ac78f8
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-01-02 18:25:00 +0100

    nvim: update lua line

--- files/nvim/nvim/lua/jpbruinsslot/plugins/lualine.lua
+++ files/nvim/nvim/lua/jpbruinsslot/plugins/lualine.lua
@@ -1,7 +1,41 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    options = {
-        theme = "nord",
-    }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+		require("lualine").setup({
+			options = {
+				theme = "nord",
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = { "nvim-tree" },
+		})
+	end,
 }
